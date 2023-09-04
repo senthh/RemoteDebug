7adb6de35acd60fcea4f741b3efa99ee5115e736
senthilkumar <senthh@gmail.com>
2023-09-04 09:43:24 +0530

    Remote debug

---
+++ src/main/java/com/remotedebug/RemoteDebug.java
@@ -0,0 +1,18 @@
+package com.remotedebug;
+
+public class RemoteDebug {
+    public static void main(String[] args) {
+        System.out.println("Starting the remote debugging application");
+        System.out.println("The application will be running inside a docker image");
+        int i = 0;
+        while(true) {
+            System.out.println("Value of i is " + i);
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            i++;
+        }
+    }
+}
